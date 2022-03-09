396548b1be15ba7e2ecb66e321dfb33f29f06efd
sortejosh <90452731+sortejosh@users.noreply.github.com>
2022-03-09 14:46:52 +0100

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ObligatoriskOpgave_4
+{
+    public class Car
+    {
+        private int _id;
+        private string _model;
+        private int _price;
+        private string _licensePlate;
+
+
+        public Car(int id, string model, int price, string licensePlate)
+        {
+
+            Id = id;
+            Model = model;
+            Price = price;
+            LicensePlate = licensePlate;
+        }
+
+        public string LicensePlate { get; set; }
+
+        public int Price { get; set; }
+
+        public string Model { get; set; }
+
+        public int Id { get; set; }
+
+        public Car()
+        {
+        }
+    }
+}
+
